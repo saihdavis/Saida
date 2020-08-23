8b2d41d515b18c5317d9028b1278fbed33f6a9c5
saidadavis <saihdavis@gmail.com>
2020-08-23 18:37:11 -0400

    new setup

---
+++ lib/cosmetic_helper.rb
@@ -0,0 +1,6 @@
+require "cosmetic_helper/version"
+
+module CosmeticHelper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cosmetic_helper/version.rb
@@ -0,0 +1,3 @@
+module CosmeticHelper
+  VERSION = "0.1.0"
+end
